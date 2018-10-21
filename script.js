881a04d6b6d2770ff45a7bf176c2d45474e82d86
Christoph <carstens.christoph@gmail.com>
2018-10-21 17:41:45 +0200

    Added JavaScript

--- script.js
+++ script.js
@@ -1,52 +1,101 @@
 console.clear()
 
+// create class constructor
 class Dog {
+  // call functions
   constructor () {
-    // call function in constructor with this
     this.createElement()
     this.changeBackground()
     this.addInteractivity()
     this.addBarkingEvents()
+    this.startMoveTimeout()
+    this.addEyes()
+    this.randomBlinking()
   }
-  // save new element into this not const
+  // create new div with class 'dog' and insert into body
   createElement () {
     this.el = document.createElement('div')
     this.el.classList.add('dog')
     document.body.insertAdjacentElement('beforeend', this.el)
   }
-  // we have to make function to create random nr. because we render new number 3 times.
-  // some action needs to take place here
+  // change Background of <div>
   changeBackground () {
-    const rand = () => Math.round(Math.random()* 255)
-    this.el.style.background = `rgb(${rand()},${rand()},${rand()})`
+    // generate random color
+    const rand = () => Math.round(Math.random() * 255)
+    // place random color as background
+    this.el.style.background = `rgb(${rand()}, ${rand()}, ${rand()})`
   }
+
   addInteractivity () {
     this.el.addEventListener('click', () => {
       this.changeBackground()
     })
   }
+
   addBarkingEvents () {
     this.el.addEventListener('mouseover', () => {
       this.startBarking()
     })
+
     this.el.addEventListener('mouseout', () => {
       this.stopBarking()
     })
   }
+
   startBarking () {
     this.startBark = document.createElement('div')
     this.startBark.classList.add('bark')
     this.startBark.textContent = 'WAU WAU!'
     this.el.insertAdjacentElement('afterbegin', this.startBark)
   }
+
   stopBarking () {
     this.startBark.remove()
   }
 
+  startMoveTimeout () {
+    setTimeout(() => {
+      this.move()
+      this.startMoveTimeout()
+    }, 1000)
+  }
 
+  move () {
+    this.el.style.right = this.getRandomPosition()
+  }
 
+  getRandomPosition () {
+    const rand = Math.round(Math.random()*80)-40
+    return `${rand}px`
+  }
+
+  addEyes () {
+  this.newEyes = [new Eye(this.el), new Eye(this.el)]
+  }
+
+  randomBlinking () {
+  setTimeout( () => {
+    this.newEyes.forEach(eye => eye.blink())
+    this.randomBlinking()
+  }, 1000 + Math.random()*3000)
+  }
 }
 
-new Dog()
+class Eye {
+  constructor(target) {
+    this.el = document.createElement('div')
+    this.el.classList.add('eye')
+    target.insertAdjacentElement('afterbegin', this.el)
+  }
+
+  blink () {
+    this.el.style.transform = 'scale(1,0.1)'
+    setTimeout( () => {
+      this.el.style.transform = 'scale(1,1)'
+    }, 200)
+  }
+}
+
+// call instances
 new Dog()
 new Dog()
